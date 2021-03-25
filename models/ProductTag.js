1d30fc4127ba8c8ea47202f99d27054adc43dabe
SageTengu <kyle.kleven@outlook.com>
2021-03-25 01:43:18 -0500

    Fixed tag_id not getting seeded (missing mustache)

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -18,13 +18,13 @@ ProductTag.init(
       references: {
         model: 'product',
         key: 'id',
-      },
-      tag_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'tag',
-          key: 'id',
-        }
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
       }
     }
   },
